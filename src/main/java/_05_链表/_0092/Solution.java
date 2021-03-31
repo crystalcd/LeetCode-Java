a1a3731ef80f32f1d79ad8e35caa14b3b7e2b25a
crystal <1627566612@qq.com>
2021-03-31 11:07:27 +0800

    添加反转链表 迭代 递归

--- src/main/java/_05_链表/_0092/Solution.java
+++ src/main/java/_05_链表/_0092/Solution.java
@@ -3,13 +3,85 @@ package _05_链表._0092;
 import data.struct.linked.ListNode;
 
 public class Solution {
+    ListNode successor = null; // 记录后继节点
+    /**
+     *  迭代
+     *  先找到需要转换的节点cur前的一个节点为pre, 当前cur的下一个节点为tmp，让cur的next指向tmp的next，
+     *  tmp的next指向pre的next，pre的next执行tmp。
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head; // 构造虚拟节点存储初始头节点
-        while(head!=null) {
-            head = head.next;
+        // 当head=left时 将head.next放到head前直到 head=right后
+        if(head==null) {
+            return head;
+        }
+        ListNode predecessor = new ListNode(-1,head);// 前驱
+        ListNode dummy = predecessor; // 虚拟节点 用来记录最后返回的头节点
+        int count=0;
+        while(count<left-1) {
+            ++count;
+            predecessor = predecessor.next;
+        }
+        ListNode cur = predecessor.next;
+        while(count<right-1) {
+            ListNode tmp = cur.next;
+            cur.next = tmp.next;
+            tmp.next = predecessor.next;
+            predecessor.next = tmp;
+            System.out.println(cur.val+" "+tmp.val+"  "+predecessor.val);
+            ++count;
         }
         return dummy.next;
+    }
+
+    /**
+     * 递归
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween1(ListNode head, int left, int right) {
+        if(left==1) {
+            return reverseBetween1(head,right);
+        }
+        head.next = reverseBetween1(head.next,left-1,right-1);
+        return head;
+    }
+
+    /**
+     * 递归反转整个链表
+     * @param head
+     * @return
+     */
+    public ListNode reverseBetween1(ListNode head) {
+        if(head.next==null) { // 只有头节点 返回头
+            return head;
+        }
+        // head.next为起点 反转前所有节点 返回反转之后的头节点
+        ListNode last = reverseBetween1(head.next);
+        head.next.next=head;
+        head.next = null;
+        return last;
+    }
+    /**
+     * 递归反转链表的前n个几点
+     * @param head
+     * @return
+     */
+    public ListNode reverseBetween1(ListNode head,int n) {
+        if(n==1) {
+            successor = head.next;
+            return head;
+        }
+        // head.next为起点 反转前n-1个节点 返回反转之后的头节点
+        ListNode last = reverseBetween1(head.next,n-1);
+        head.next.next=head;
+        head.next =successor;
+        return last;
     }
 }
 
--- src/main/java/_05_链表/_0092/SolutionTest.java
+++ src/main/java/_05_链表/_0092/SolutionTest.java
@@ -17,12 +17,48 @@ class SolutionTest {
     @MethodSource("_0092")
     void reverseBetween(int[] input,int left,int right, int[] output) {
         ListNode head = ListNode.array2Link(input);
-        ListNode rs = new Solution().reverseBetween(head, left, right);
+        ListNode rs = new Solution().reverseBetween1(head, left, right);
+        int[] actuals = ListNode.link2array(rs);
+        assertArrayEquals(output,actuals);
+
     }
+
+    @ParameterizedTest
+    @MethodSource("_0092_1")
+    void reverseBetween1(int[] input,int[] output) {
+        ListNode head = ListNode.array2Link(input);
+        ListNode rs = new Solution().reverseBetween1(head);
+        int[] actuals = ListNode.link2array(rs);
+        assertArrayEquals(output,actuals);
+    }
+
+    @ParameterizedTest
+    @MethodSource("_0092_3")
+    void reverseBetween1(int[] input,int[] output,int n) {
+        ListNode head = ListNode.array2Link(input);
+        ListNode rs = new Solution().reverseBetween1(head,n);
+        int[] actuals = ListNode.link2array(rs);
+        assertArrayEquals(output,actuals);
+    }
+
+    static Stream<Arguments> _0092_1() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, new int[]{5,4,3,2,1}),
+                Arguments.of(new int[]{3, 5},new int[]{5,3})
+        );
+    }
+
 
     static Stream<Arguments> _0092() {
         return Stream.of(
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, 2,4, new int[]{1,4,3,2,5})
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 2,4, new int[]{1,4,3,2,5}),
+                Arguments.of(new int[]{3, 5}, 1,2, new int[]{5,3})
+        );
+    }
+
+    static Stream<Arguments> _0092_3() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, new int[]{3,2,1,4,5},3)
         );
     }
 }
